d9b92b44ba76234fd9e6989732b067d150e2e9a0
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2026-02-20 20:10:06 -0800

    exerciae

--- exercises/ex01.js
+++ exercises/ex01.js
@@ -2,10 +2,21 @@
 // Tip: Create two runners - slow and fast
 // (The slow runner moves one node at a time, the fast runner moves two nodes at a time. When the fast runner is at the very last node, then the slow runner will eventually be in the middle of the list. You can then return its value)
 
-const SLL = require('../lib/SLL');
+const SLL = require("../lib/SLL");
 
 function findMiddle(list) {
   // your code here
+  let slowRunner = list.getHead();
+  let fastRunner = list.getHead();
+  let middleNumber = null;
+
+  while (slowRunner !== null && fastRunner.next !== null) {
+    slowRunner = slowRunner.next;
+    fastRunner = fastRunner.next.next;
+  }
+  middleNumber = slowRunner.data;
+
+  return middleNumber;
 }
 
 const list = new SLL();
--- exercises/ex02.js
+++ exercises/ex02.js
@@ -1,10 +1,34 @@
 // Create a function called deleteAllNodesWithValue that removes all nodes with a specific value
 // Make sure to update the size of the list as well
 
-const DLL = require('../lib/DLL');
+const DLL = require("../lib/DLL");
 
 function deleteAllNodesWithValue(list, value) {
   // your code here
+  // let runner = list.getHead();
+  let listSize = list.length();
+
+  // while (runner !== null) {
+  // if (runner.data !== value) {
+  //   runner = runner.next;
+  // }
+  // runner = runner.next.next;
+  // listSize -= 1;
+  if (list.getHead().data === value) {
+    list.getHead() = list.getHead().next;
+    listSize -= 1;
+  }
+  // runner = runner.next;
+  let runner = list.getHead();
+  // }
+
+  while (runner.next !== null) {
+    if (runner.next.data === value) {
+      runner = runner.next.next;
+      listSize -= 1;
+    }
+    runner = runner.next;
+  }
 }
 
 const list = new DLL();
